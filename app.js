50a994a2fd8d2e3f7926b4e35ca2907448ae021b
abenosan00 <dazdazdaz00@yahoo.co.jp>
2018-08-22 21:27:51 +0900

    ejs,Express追加

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'); 
+var ejs = require('ejs');
+
+var app = express();
+
+app.engine('ejs',ejs.renderFile);
+
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs',{
+        title:'index',
+        content:'Hello Wrold',
+    })
+})
+
+var server = app.listen(3000,()=>{
+    console.log("server.start");
+});
